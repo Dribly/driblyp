5ef77ad4c29152bedb5755be7191b6039e446a63
Toby Powell-Blyth <toby@thegiltclub.com>
2018-06-28 14:42:26 +0100

    Remove comments

--- tests/Unit/Library/Services/CloudMQTTTest.php
+++ tests/Unit/Library/Services/CloudMQTTTest.php
@@ -7,28 +7,13 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CloudMQTTTest extends TestCase
 {
-//        public static function makeFeedName(int $type, string $uid) {
-//        $cleanUID = replace('/','',$uid);
-//        return replace('/uid/','/'.$cleanUID.'/', self::FEED_TYPES[$type]);
-//    }
-
     public function providerMakeFeedName()
     {
-        
-//        self::FEED_WATERSENSOR => "dribly/watersensors/uid/update",
-//        self::FEED_WATERSENSORIDENTIFY => "dribly/watersensors/uid/identify",
-//        self::FEED_TAP => "dribly/taps/uid/update",
-//        self::FEED_TAPIDENTIFY => "dribly/taps/uid/identify"
-        
         return [
             [\App\Library\Services\CloudMQTT::FEED_WATERSENSOR, 'fakeuid', 'dribly/watersensors/fakeuid/update'],
             [\App\Library\Services\CloudMQTT::FEED_WATERSENSORIDENTIFY, '2akeuid', 'dribly/watersensors/2akeuid/identify'],
             [\App\Library\Services\CloudMQTT::FEED_TAP, 'fakeuid', 'dribly/taps/fakeuid/update'],
             [\App\Library\Services\CloudMQTT::FEED_TAPIDENTIFY, 'fakeuid', 'dribly/taps/fakeuid/identify'],
-//    const FEED_WATERSENSORIDENTIFY = 11;
-//    const FEED_TAP = 20;
-//    const FEED_TAPIDENTIFY = 21;
-
         ];
     }
     /**
