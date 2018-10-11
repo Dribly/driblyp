0b7228ac20d9c9188d3917dbe887b9f1e02cb58f
Toby Powell-Blyth <toby@thegiltclub.com>
2018-10-11 15:41:39 +0100

    Add google tag manager

--- resources/views/layouts/material2.blade.php
+++ resources/views/layouts/material2.blade.php
@@ -16,9 +16,22 @@
     <!-- CSS Files -->
     <link href="../themes/material2/css/material-dashboard.css?v=2.1.0" rel="stylesheet"/>
     <link href="../themes/material2/css/app.css" rel="stylesheet"/>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-M79SC8L');</script>
+    <!-- End Google Tag Manager -->
+
 </head>
 
 <body class="">
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M79SC8L"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
 <div class="wrapper ">
     <div class="sidebar" data-color="@yield('pageColour', 'purple')" data-background-color="white"
          data-image="../themes/material2/img/sidebar-1.jpg">
